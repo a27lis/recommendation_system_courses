8c5cc61f368632f024b3b89dd7b5308cf7c9c94e
a27lis <anastasiya.liseykina@gmail.com>
2024-04-24 18:32:29 +0400

    correct input

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
+import pandas as pd
+
 name = "main"
 app = Flask(name)
 
-@app.route('/')
-def hello_world():
-  return render_template('index.html')
+data = pd.read_csv("C:/src/recommendation_system/BigData_project/data/Coursera.csv")
+data = data[['Course Name', 'Difficulty Level', 'Course Description', 'Skills']]
+
+
+@app.route('/', methods=["POST", "GET"])
+def recommend_system():
+    if request.method == "GET":
+        return render_template('index.html', data=data['Course Name'].to_list())
+
 
 if name == 'main':
-  app.run(debug=True)
+    app.run(debug=True)
--- static/show.js
+++ static/show.js
@@ -1,6 +1,6 @@
 let content = document.getElementById("courses-block")
 let show = document.getElementById("show-btn")
-let form = document.getElementById("formControlSelect")
+let form = document.getElementById("tags")
 
 show.addEventListener("click", () => {
     content.style.display = "block"
@@ -11,22 +11,4 @@ form.addEventListener("click", () => {
 })
 
 
-var options = ["Cat", "Dog", "Banana", "Apple", "Car", "Boat"]; // Массив с возможными значениями
 
-function filterOptions() {
-  var input = document.getElementById("myInput");
-  var filter = input.value.toUpperCase();
-  var dropdown = document.getElementById("dropdown");
-  dropdown.innerHTML = "";
-  for (var i = 0; i < options.length; i++) {
-    if (options[i].toUpperCase().indexOf(filter) > -1) {
-      var option = document.createElement("div");
-      option.textContent = options[i];
-      option.onclick = function(e) {
-        input.value = this.textContent;
-        dropdown.innerHTML = "";
-      };
-      dropdown.appendChild(option);
-    }
-  }
-}
